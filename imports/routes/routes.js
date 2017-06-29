34ae889c23bed1f6fd0e4e771a03c9291505d046
Adrian Patrick <Adrianpn@gmail.com>
2017-06-29 07:29:42 -0400

    Mark selected note in list

--- imports/routes/routes.js
+++ imports/routes/routes.js
@@ -1,13 +1,14 @@
 import { Meteor } from 'meteor/meteor';
 import React from 'react';
 import { Router, Route, browserHistory } from 'react-router';
+import { Session } from 'meteor/session';
 
 import  Login  from '../ui/Login';
 import  Signup  from '../ui/Signup';
 import  Dashboard  from '../ui/Dashboard';
 import  NotFound  from '../ui/NotFound';
 
-window.browserHistory = browserHistory;
+//window.browserHistory = browserHistory;
 
 const unauthenticatedPages = ['/','/signup'];
 const authenticatedPages = ['/Dashboard'];
@@ -19,6 +20,13 @@ const onEnterPublicPage = () => {
 const onEnterPrivatePage = () => {
   if (!Meteor.userId()){
     browserHistory.replace('/');
+  }
+};
+const onEnterNotePage = (nextState) => {
+  if (!Meteor.userId()){
+    browserHistory.replace('/');
+  } else {
+    Session.set('selectedNoteId', nextState.params.id);
   }
 };
 export const onAuthChange = (isAuthenticated) => {
@@ -38,7 +46,7 @@ export const routes = (
     <Route path="/" component={Login} onEnter={onEnterPublicPage}/>
     <Route path="/signup" component={Signup} onEnter={onEnterPublicPage}/>
     <Route path="/Dashboard" component={Dashboard} onEnter={onEnterPrivatePage}/>
-    <Route path="/Dashboard/:id" component={Dashboard} onEnter={onEnterPrivatePage}/>
+    <Route path="/Dashboard/:id" component={Dashboard} onEnter={onEnterNotePage}/>
     <Route path="*" component={NotFound} onEnter={onEnterPrivatePage}/>
   </Router>
 );
